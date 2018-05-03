c8ddbef2a5b74ca045233382256b26d9f796a4d5
sergiopoliveira <sergiopoliveira@outlook.com>
2018-05-03 17:40:55 +0100

    Repositories and data.sql

--- src/main/java/com/recipe/domain/Recipe.java
+++ src/main/java/com/recipe/domain/Recipe.java
@@ -4,10 +4,15 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
@@ -33,14 +38,26 @@ public class Recipe {
 	@Lob 
 	private Byte[] image;
 	
+	@Enumerated(value = EnumType.STRING)
+	private Difficulty difficulty;
+	
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	private Notes notes;
+	
+	@ManyToMany
+	@JoinTable(name = "recipe_category", 
+			joinColumns = @JoinColumn(name = "recipe_id"), 
+			inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories;
+	
+	
 	public Long getId() {
 		return id;
 	}
 	public void setId(Long id) {
 		this.id = id;
 	}
-	@OneToOne(cascade = CascadeType.ALL)
-	private Notes notes;
 	
 	public String getDescription() {
 		return description;
@@ -89,6 +106,24 @@ public class Recipe {
 	}
 	public void setNotes(Notes notes) {
 		this.notes = notes;
+	}
+	public Set<Ingredient> getIngredient() {
+		return ingredient;
+	}
+	public void setIngredient(Set<Ingredient> ingredient) {
+		this.ingredient = ingredient;
+	}
+	public Difficulty getDifficulty() {
+		return difficulty;
+	}
+	public void setDifficulty(Difficulty difficulty) {
+		this.difficulty = difficulty;
+	}
+	public Set<Category> getCategories() {
+		return categories;
+	}
+	public void setCategories(Set<Category> categories) {
+		this.categories = categories;
 	}
 	
 	
