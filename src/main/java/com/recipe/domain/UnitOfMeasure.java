9b1422af96a849cf1dcb2f70b8e5432a8cdcb1eb
sergiopoliveira <sergiopoliveira@outlook.com>
2018-05-03 14:53:05 +0100

    Entities and index template

---
+++ src/main/java/com/recipe/domain/UnitOfMeasure.java
@@ -0,0 +1,35 @@
+package com.recipe.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+@Entity
+public class UnitOfMeasure {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Lob
+	private String description;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUom() {
+		return description;
+	}
+
+	public void setUom(String description) {
+		this.description = description;
+	}
+
+}
