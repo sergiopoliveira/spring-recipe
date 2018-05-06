f0be18998016d40dd2abb03c0964e7f4cfcb358e
sergiopoliveira <sergiopoliveira@outlook.com>
2018-05-06 16:08:32 +0100

    Display recipe properties

--- src/main/java/com/recipe/bootstrap/RecipeBootStrap.java
+++ src/main/java/com/recipe/bootstrap/RecipeBootStrap.java
@@ -165,6 +165,10 @@ public class RecipeBootStrap implements ApplicationListener<ContextRefreshedEven
 
 		guacRecipe.getCategories().add(americanCategory);
 		guacRecipe.getCategories().add(mexicanCategory);
+		
+		guacRecipe.setUrl("https://www.simplyrecipes.com/recipes/perfect_guacamole/");
+		guacRecipe.setServings(4);
+		guacRecipe.setSource("Simply Recipes");
 
 		// add to return list
 		recipes.add(guacRecipe);
--- src/test/java/com/recipe/springrecipe/controllers/RecipeControllerTest.java
+++ src/test/java/com/recipe/springrecipe/controllers/RecipeControllerTest.java
@@ -15,6 +15,7 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
 public class RecipeControllerTest {
 
@@ -42,7 +43,8 @@ public class RecipeControllerTest {
 		
 		mockMvc.perform(get("/recipe/show/1"))
 			.andExpect(status().isOk())
-			.andExpect(view().name("recipe/show"));
+			.andExpect(view().name("recipe/show"))
+			.andExpect(model().attributeExists("recipe"));
 
 	}
 
