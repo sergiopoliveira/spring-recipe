881e99a7cbdd77e10bda36e1222e70e0243ce1c6
sergiopoliveira <sergiopoliveira@outlook.com>
2018-05-31 15:14:44 +0100

    Data Validation with Spring MVC

--- src/main/java/com/recipe/controllers/RecipeController.java
+++ src/main/java/com/recipe/controllers/RecipeController.java
@@ -1,11 +1,12 @@
 package com.recipe.controllers;
 
-import java.text.NumberFormat;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -24,6 +25,8 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class RecipeController {
 
+	private static final String RECIPE_RECIPEFORM_URL = "recipe/recipeform";
+	
 	private final RecipeService recipeService;
 
 	@Autowired
@@ -44,17 +47,27 @@ public class RecipeController {
 		
 		model.addAttribute("recipe", new RecipeCommand());
 		
-		return "recipe/recipeform";
+		return RECIPE_RECIPEFORM_URL;
 	}
 	
 	@GetMapping("recipe/{id}/update")
 	public String updateRecipe(@PathVariable String id, Model model) {
 		model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
-		return "recipe/recipeform";
+		return RECIPE_RECIPEFORM_URL;
 	}
 	
 	@PostMapping("recipe")
-	public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+	public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult) {
+		
+		if(bindingResult.hasErrors()) {
+			
+			bindingResult.getAllErrors().forEach(objectError -> {
+				log.debug(objectError.toString());
+			});
+			
+			return RECIPE_RECIPEFORM_URL;
+		}
+		
 		RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 		
 		return "redirect:/recipe/" + savedCommand.getId() + "/show";
